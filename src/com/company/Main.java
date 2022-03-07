9cc5e215215f897ef9fff6bd64d2ac068cc3aff7
DavidGabelgaard <davi493c@edu.easj.dk>
2022-03-07 14:28:59 +0100

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,75 @@
 package com.company;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(recursivePos(4,4));
 
-        System.out.println(pow(4,4));
-
-        System.out.println(fak(5));
+        System.out.println(recursiveFak(5));
 
         System.out.println(fib(12));
+
+        System.out.println(secondPow(5));
+
+        System.out.println(addNumbersUpTo(5));
+
+        int [] array = new int[] {1 , 3 , 5, 1, 7 , 1};
+
+        //System.out.println(count(  1 , array , array.length -1    ));
+
+         reverseDisplay("Marc");
+
+        reverseDisplayInt(array , array.length -1 );
+
+    }
+
+    public void dec2Hex() {
+
 
 
     }
 
+    public static void reverseDisplay(String s) {
+        System.out.println();
+
+        for (int i = s.length() - 1; i >= 0 ; i--) {
+            System.out.print(s.charAt(i));
+        }
+
+    }
+
+    public static void reverseDisplayInt(int[] array , int index) {
+        if (index != -1) {
+            System.out.println(array[index]);
+            reverseDisplayInt(array, index - 1);
+        }
+    }
+
+    public static int count(int lookFor, int[] array, int arraySize ) {
+        int result  = 0;
+
+        if (arraySize >= 0)
+            result = ( count(lookFor , array , arraySize - 1) + ( ( lookFor == array[arraySize] ) ? 1 : 0 ) );
+
+        return  result;
+    }
+
+    public static int addNumbersUpTo(int n) {
+        if (n == 1)
+            return 1;
+        else
+            return n + addNumbersUpTo(n-1);
+    }
+
+    public static int secondPow(int n) {
+        if (n == 1)
+            return 2;
+        else
+            return 2 * secondPow(n -1);
+    }
 
     public static int pow (int x, int n) {
         int timesValue = x;
@@ -25,7 +81,6 @@ public class Main {
         }
         return  x;
     }
-
 
     public static int fak(int n) {
         int sum = n;
@@ -54,6 +109,28 @@ public class Main {
         return returnNumber;
     }
 
+    public static  int recursivePos(int x , int n) {
+        if (n == 0)
+            return 1;
+        else
+            return x*recursivePos(x,n-1);
+        }
+
+    public static int recursiveFak(int n) {
+
+            if (n == 1)
+                return  1;
+            else
+                return n * recursiveFak(n - 1);
 
 
-}
+
+        }
+
+
+
+    }
+
+
+
+
